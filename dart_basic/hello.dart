7c99dea05733bd4f997ecad4c7ded46fbcb39c15
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-12-05 22:29:27 +0700

    first commit

---
+++ dart_basic/hello.dart
@@ -0,0 +1,4 @@
+void main() {
+  String name = "Irfan";
+  print("Hello, I'm $name");
+}
---
+++ dart_basic/variabel.dart
@@ -0,0 +1,14 @@
+// variabel digunakan untuk menyimpan value,
+
+// Variabel Decalration Vs Variabel Initialization
+
+// Variabel declaration = proses unuk mencari variabel dan memberikan nama
+
+// Vaiabel initialization = proses assigning an initial value variabel
+
+void main() {
+  String name = "Irfan"; // variabel
+  print("Hello, I'm $name");
+  // String interpolation
+  print("My name has ${name.length} letters");
+}
